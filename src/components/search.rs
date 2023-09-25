9b05bd6ae9b07b5b464ac16651b5147c1946ddf8
Sugi <developer.sugiono@gmail.com>
2023-09-25 18:00:53 +0700

    add debounce time

--- src/components/search.rs
+++ src/components/search.rs
@@ -12,6 +12,7 @@ pub enum SearchInputMsg {
 
 pub struct SearchInput {
     input_ref: NodeRef,
+    debounce_timer: Option<i32>,
 }
 
 impl Component for SearchInput {
@@ -22,6 +23,7 @@ impl Component for SearchInput {
     fn create(_ctx: &yew::Context<Self>) -> Self {
         Self {
             input_ref: NodeRef::default(),
+            debounce_timer: None,
         }
     }
 
@@ -29,7 +31,18 @@ impl Component for SearchInput {
         match msg {
             SearchInputMsg::Search(_) => {
                 if let Some(input) = self.input_ref.cast::<HtmlInputElement>() {
-                    ctx.props().onsearch.emit(input.value());
+                    if let Some(timeout_id) = self.debounce_timer.take() {
+                        gloo_utils::window().clear_timeout_with_handle(timeout_id);
+                    }
+
+                    let callback = ctx.props().onsearch.clone();
+
+                    self.debounce_timer = gloo::timers::callback::Timeout::new(500, move || {
+                        callback.emit(input.value());
+                    })
+                    .forget()
+                    .as_f64()
+                    .map(|t| t as i32);
                 }
                 false
             }
